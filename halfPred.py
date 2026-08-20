20b8251f64be5fff86158e381afd497b56dfaf5b
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2026-08-19 22:50:54 -0500

    Add function for descriptive stats on df

--- halfPred.py
+++ halfPred.py
@@ -1,6 +1,7 @@
 from kagglehub import KaggleDatasetAdapter
 from dotenv import load_dotenv
 import pandas as pd
+import numpy as np
 
 # Have to load the environment variable prior to loading the kagglehub and kaggle libraries in python
 load_dotenv()
@@ -8,14 +9,53 @@ import kagglehub
 import kaggle
 kaggle.api.authenticate()
 
-# kagglehub.login()
+# Setting script variables
+ACCT_STRING = "saife245/english-premier-league"
+DF_STRING = "final_dataset.csv"
 
-# Load a DataFrame with a specific version of a CSV
-df = kagglehub.dataset_load(
-    KaggleDatasetAdapter.PANDAS,
-    "saife245/english-premier-league",
-    "final_dataset.csv"
-)
+def kaggleDfLoader(acctStr: str, dfStr: str):
+    # Load a DataFrame with a specific version of a CSV
+    df = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        acctStr,
+        dfStr
+    )
 
-print(df.head())
-print(df.shape)
+    return df
+
+def dfDescriber(df) -> None:
+    print(df.head())
+    print("-"*60)
+    print()
+
+    shape = df.shape
+    print(f"This dataframe has {shape[0]} rows and {shape[1]} columns.")
+    print("-"*60)
+    print()
+
+    print(df.isna().sum())
+    print("-"*60)
+    print()
+
+    print(df.dtypes)
+    print("-"*60)
+    print()
+
+    for dtype in df.dtypes.drop_duplicates():
+        print(f"There are {sum([1 if dt==dtype else 0 for dt in df.dtypes])} columns of the {dtype} data-type")
+    print("-"*60)
+    print()
+
+
+    
+
+def main():
+    # loading descriptive statistics
+    df = kaggleDfLoader(ACCT_STRING, DF_STRING)
+    dfDescriber(df)
+
+    # looking into the string columns
+
+
+if __name__ == "__main__":
+    main()
